78a06bb231b857d3bf7bf7e294d81d00025b7f3d
klips-4 <klips-4@yandex.ru>
2022-11-16 19:09:18 +0300

    16.11.2022

---
+++ src/components/Task/TaskRender.vue
@@ -0,0 +1,26 @@
+<template src="./template.html"/>
+
+<script>
+
+
+    export default {
+        name: "TaskRender",
+        props: ['data'],
+        data() {
+            return {
+
+            }
+
+        },
+
+        methods: {
+            deleteItem() {
+
+            }
+        }
+
+
+    }
+</script>
+
+<style scoped src="./style.less" lang="less"></style>
--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -3,21 +3,37 @@
 <script>
     import {AuthHelpers} from "@/helpers/AuthHelpers";
     import AddForm from "@/pages/AddForm/AddForm";
+    import SourceService from "@/services/SourceService";
+    import TaskRender from "@/components/Task/TaskRender";
 
     export default {
         name: "HomePage",
-        components: {AddForm},
+        components: {AddForm, TaskRender},
+
         data() {
             return {
-                addFormVisible: false
+                addFormVisible: false,
+                items: [],
+                source: new SourceService({endpoint: 'Task'})
             }
         },
+        beforeMount() {
+            this.getItems()
+        },
+
         methods: {
             logout() {
                 AuthHelpers.logout();
             },
             changeAddFormVisible() {
                 this.addFormVisible = !this.addFormVisible;
+            },
+            getItems() {
+                this.source.list().then((result) => {
+                    if (result.success) {
+                        this.items = result.data;
+                    }
+                });
             }
         },
         computed: {
@@ -25,7 +41,9 @@
                 const userData = AuthHelpers.getUserInfo();
                 return userData.surname + ' ' + userData.name[0] + '.';
             }
-        }
+        },
+
+
     }
 </script>
 
